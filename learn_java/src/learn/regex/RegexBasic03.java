91577c496402cf6642001250798bdd52734402e4
jitaek <jitaek@Lecturer-PC>
2019-03-06 09:11:37 +0900

    add regex demos

---
+++ learn_java/src/learn/regex/RegexBasic03.java
@@ -0,0 +1,14 @@
+package learn.regex;
+
+import java.util.regex.Pattern;
+
+public class RegexBasic03 {
+
+	public static void main(String[] args) {
+		System.out.println(Pattern.matches("[amn]", "abcd"));// false (not a or m or n)
+		System.out.println(Pattern.matches("[amn]", "a"));// true (among a or m or n)
+		System.out.println(Pattern.matches("[amn]", "ammmna"));// false (m and a comes more than once)
+
+	}
+
+}
