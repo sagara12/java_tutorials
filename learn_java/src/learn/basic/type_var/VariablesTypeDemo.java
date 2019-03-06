2be7e47ad22f37d91a7722729cb559e0e29f65c6
jitaek <jitaek@Lecturer-PC>
2019-03-06 09:00:44 +0900

    create learn_java project

---
+++ learn_java/src/learn/basic/type_var/VariablesTypeDemo.java
@@ -0,0 +1,13 @@
+package learn.basic.type_var;
+
+public class VariablesTypeDemo {
+
+	int data = 50;
+	static int sVar = 10;
+	
+	public static void main(String[] args) {
+		int localVar = 1;
+
+	}
+
+}
